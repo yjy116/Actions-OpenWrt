87d7b7378fd12e859995eb7a5fcaca80405785c5
yjy116 <yjy116@gmail.com>
2026-03-27 10:07:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-# 在 feeds.conf.default 最后添加插件源
+# 添加常用的第三方插件源
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+# 如果需要最新版的 HomeProxy 也可以单独拉取（可选）
+# git clone https://github.com/immortalwrt/homeproxy package/homeproxy
