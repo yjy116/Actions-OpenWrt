6381cdbaf8a7a249cadfff3487480f2b18d319c1
yjy116 <yjy116@gmail.com>
2026-03-28 06:15:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 
-# 1. 修改默认 IP 为 192.168.18.1
+# 1. 修改默认 IP
 sed -i 's/192.168.1.1/192.168.18.1/g' package/base-files/files/bin/config_generate
 
-# 2. 修改默认主机名为 Beryl7-LEDE
-sed -i 's/OpenWrt/Beryl7-LEDE/g' package/base-files/files/bin/config_generate
+# 2. 修改主机名
+sed -i 's/OpenWrt/Beryl7-Immortal/g' package/base-files/files/bin/config_generate
 
 # 3. 设置默认主题为 Argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# 4. 强制删除预设无线配置 (解决双频 WiFi 识别不全的关键)
-# 这一步会让系统在首次开机时根据驱动重新探测 radio0 和 radio1
-rm -f package/base-files/files/etc/config/wireless
+# 4. 首次启动时重新生成无线配置
+mkdir -p package/base-files/files/etc/uci-defaults/
+cat <<'EOF' > package/base-files/files/etc/uci-defaults/99-init-settings
+#!/bin/sh
 
-# 5. 开启硬件转发优化
-echo "CONFIG_NET_MEDIATEK_SOC_WED=y" >> .config
+sleep 5
 
-# 强制设置 root 密码为 password 使用lede默认密码 "password"
-# echo "password" | (passwd root <<EOF password password EOF)
+# 强制重新生成无线配置，避免旧配置或探测异常
+rm -f /etc/config/wireless
+wifi config
 
-# 6. 修改版本号标识 (可选)
-sed -i "s/OpenWrt /Built by YourName $(date +%Y-%m-%d) /g" package/base-files/files/etc/banner
+# 先只做最基本的启用和区域设置
+uci set wireless.radio0.disabled='0'
+uci set wireless.radio1.disabled='0'
+uci set wireless.radio0.country='US'
+uci set wireless.radio1.country='US'
+
+# 不要在这里写 bandwidth，OpenWrt 常用的是 htmode
+# 160MHz 建议等确认哪个 radio 是 5G 后再定向设置
+
+uci commit wireless
+exit 0
+EOF
+
+chmod +x package/base-files/files/etc/uci-defaults/99-init-settings
