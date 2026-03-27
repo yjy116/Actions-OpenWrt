c46676fed1af1348915983ab2254818aa9a191ae
yjy116 <yjy116@gmail.com>
2026-03-27 16:13:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# 1. 修改默认 IP 为 192.168.8.1 (GL.iNet 用户习惯)
-sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+# 1. 修改默认 IP 为 192.168.18.1
+sed -i 's/192.168.1.1/192.168.18.1/g' package/base-files/files/bin/config_generate
 
 # 2. 修改默认主机名为 Beryl7-LEDE
 sed -i 's/OpenWrt/Beryl7-LEDE/g' package/base-files/files/bin/config_generate
@@ -9,13 +9,14 @@ sed -i 's/OpenWrt/Beryl7-LEDE/g' package/base-files/files/bin/config_generate
 # 3. 设置默认主题为 Argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# 4. 强制开启硬件加速相关的内核定义 (针对 Filogic 平台)
+# 4. 强制删除预设无线配置 (解决双频 WiFi 识别不全的关键)
+# 这一步会让系统在首次开机时根据驱动重新探测 radio0 和 radio1
+rm -f package/base-files/files/etc/config/wireless
+
+# 5. 开启硬件转发优化
 echo "CONFIG_NET_MEDIATEK_SOC_WED=y" >> .config
 
-# 5. 移除 root 默认密码 (刷机后初次登录无需密码)
-# sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CY6Sv6wSRCosSKqvePqgr0:18856:0:99999:7:::/g' package/base-files/files/etc/shadow
-
-# 强制设置 root 密码为 password
+# 强制设置 root 密码为 password 使用lede默认密码 "password"
 # echo "password" | (passwd root <<EOF password password EOF)
 
 # 6. 修改版本号标识 (可选)
