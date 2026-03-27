750dd306d8ebe43d6a23d15adcaa36359e9a8a42
yjy116 <yjy116@gmail.com>
2026-03-28 06:13:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-# 添加常用的第三方插件源
+# 1. 添加第三方插件源 (kenzo 仓库包含 OpenClash, MosDNS 等)
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
-# 如果需要最新版的 HomeProxy 也可以单独拉取（可选）
-# git clone https://github.com/immortalwrt/homeproxy package/homeproxy
+# 2. 【核心变更】确保 HomeProxy 使用 ImmortalWrt 原生推荐版本
+# 这样可以避免编译时出现 sing-box 依赖版本不匹配的报错
+rm -rf package/feeds/luci/luci-app-homeproxy
+git clone https://github.com/immortalwrt/homeproxy package/homeproxy
